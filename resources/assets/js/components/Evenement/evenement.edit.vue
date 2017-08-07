34a1f49d9d2c01ca18c97d7abda661045ab24df0
Patrois <emmanuel.patrois@etu.u-bordeaux.fr>
2017-08-07 20:55:04 +0200

    edit

--- resources/assets/js/components/Evenement/evenement.edit.vue
+++ resources/assets/js/components/Evenement/evenement.edit.vue
@@ -4,13 +4,13 @@
             <div class="card">
                 <div class="card-header card-header-text" data-background-color="purple">
                     <!-- <i class="material-icons">event_note</i> -->
-                    <h4 class="card-title">Modifier un évenement</h4>
+                    <h4 class="card-title">Evenement n° {{evenement.id}} - {{evenement.titre}}</h4>
                 </div>
                 <form id="formEditArticle" class="form-horizontal" v-on:submit.prevent="updateEvenement">
                     <input name="_method" type="hidden" value="PUT"> 
                     <div class="card-content">
                         <div class="row">
-                            <div class="col-sm-12 text-center">
+                            <div class="col-md-6 text-center">
                                 <div class="fileinput fileinput-new text-center" data-provides="fileinput">
                                     <div class="fileinput-new thumbnail">
                                         <img v-bind:src="evenement.affiche" alt="...">
@@ -27,50 +27,18 @@
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-12">
+                            <div class="col-md-6">
                                 <div class="form-group label-floating">
                                     <label class="control-label">Titre</label>
                                     <input input class="form-control" type="text" name="titre" v-model="evenement.titre" required>
                                 </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6">
+                             <div class="col-md-6">
                                 <div class="form-group label-floating">
                                     <label class="control-label">Lieu</label>
                                     <input class="form-control" type="text" name="lieu" v-model="evenement.lieu" required>
                                 </div>
                             </div>
-                            <div class="col-md-6">
-                                <div class="form-group label-floating">
-                                    <label class="control-label">Prix</label>
-                                    <input class="form-control" type="number" name="prix" v-model="evenement.prix" required>
-                                    
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div id="categorieControl" class="col-md-6">
-                                <div class="form-group label-floating">
-                                    <!-- <label>Catégorie</label> -->
-                                    <label id="categorieLabel" class="control-label">Catégorie</label>
-                                    <select name="categorie_id" class="selectpicker form-control" data-style="btn btn-primary btn-round"
-                                            title="">
-                                            <option v-for="categorie in categories" v-bind:key="categorie.id" v-bind:value="categorie.id">{{ categorie.nom }}</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group label-floating">
-                                    <label class="control-label">Tarifs</label>
-                                    <input class="form-control" name="tarifs" v-model="evenement.tarifs">
-                                    </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <!-- <label class="col-sm-2 label-on-left"></label> -->
                             <div class="col-md-6">
                                 <div class="form-group label-floating">
                                     <label class="control-label">Date de début</label>
@@ -84,13 +52,74 @@
                                     <input id="dtPickerEnd" name="dateFin" type="text" class="form-control datetimepicker" v-model="evenement.dateFin">
                                     </div>
                             </div>
+                            <div class="col-md-3">
+                                <div class="form-group label-floating">
+                                    <label class="control-label">Prix</label>
+                                    <input class="form-control" type="number" name="prix" v-model="evenement.prix" required>
+                                </div>
+                            </div>
+                            <div id="categorieControl" class="col-md-3">
+                                <div class="form-group label-floating">
+                                    <label id="categorieLabel" class="control-label">Catégorie</label>
+                                    <select name="categorie_id" class="selectpicker form-control" data-style="btn btn-primary btn-round"
+                                            title="">
+                                            <option v-for="categorie in categories" v-bind:key="categorie.id" v-bind:value="categorie.id">{{ categorie.nom }}</option>
+                                    </select>
+                                </div>
+                            </div>
+                             <div class="col-md-12">
+                                <div class="form-group label-floating">
+                                    <label class="control-label">Tarifs</label>
+                                    <input class="form-control" name="tarifs" v-model="evenement.tarifs">
+                                    </div>
+                            </div>
+                            
+                        </div>
+                        <div class="row">
+                            <!-- <div class="col-md-12">
+                                <div class="form-group label-floating">
+                                    <label class="control-label">Titre</label>
+                                    <input input class="form-control" type="text" name="titre" v-model="evenement.titre" required>
+                                </div>
+                            </div> -->
+                        </div>
+                        <div class="row">
+                           <!--  <div class="col-md-6">
+                                <div class="form-group label-floating">
+                                    <label class="control-label">Lieu</label>
+                                    <input class="form-control" type="text" name="lieu" v-model="evenement.lieu" required>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group label-floating">
+                                    <label class="control-label">Prix</label>
+                                    <input class="form-control" type="number" name="prix" v-model="evenement.prix" required>
+                                    
+                                </div>
+                            </div> -->
+                        </div>
+                        <div class="row">
+                            <!-- <div id="categorieControl" class="col-md-6">
+                                <div class="form-group label-floating">
+                                    <label id="categorieLabel" class="control-label">Catégorie</label>
+                                    <select name="categorie_id" class="selectpicker form-control" data-style="btn btn-primary btn-round"
+                                            title="">
+                                            <option v-for="categorie in categories" v-bind:key="categorie.id" v-bind:value="categorie.id">{{ categorie.nom }}</option>
+                                    </select>
+                                </div>
+                            </div> -->
+                           
+                        </div>
+                        <div class="row">
+                            <!-- <label class="col-sm-2 label-on-left"></label> -->
+                            
                         </div>
                         <div class="row">
                             <!-- <label class="col-sm-2 label-on-left">Description</label> -->
                             <div class="col-md-12">
                                 <div class="form-group label-floating">
                                     <label class="control-label">Description</label>
-                                    <textarea class="form-control" name="description" rows="6" cols="50">evenement.description</textarea>
+                                    <textarea class="form-control" name="description" rows="6" cols="50">{{evenement.description}}</textarea>
                                     </div>
                             </div>
                         </div>
@@ -122,7 +151,28 @@
                 <div class="card-header card-header-text" data-background-color="purple">
                     <h4 class="card-title">Commentaires</h4>
                 </div>
-                <div class="media-area">
+                <div class="">
+                    <ul class="js-comments-list comments-list">
+                        <li class="comment-container cfix user-comment" v-for="comment in comments">
+                            <a target="_blank" href="https://www.behance.net/seb90grado8237" class="rf-avatar js-mini-profile" data-id="44063465">
+                              <img v-bind:src="comment.user.avatar" class="rf-avatar__image js-avatar__image">
+                            </a>
+                              <div class="comment-text-container left relative">
+                                <div class="comment-user-date-wrap ui-corner cfix">
+                                  <a class="user-name-link bold js-mini-profile" data-id="44063465" href="https://www.behance.net/seb90grado8237">{{comment.user.name}}</a>
+                                  <span class="comment-date js-format-timestamp" data-timestamp="1501963744">{{comment.updated_at}}</span>
+                                </div>
+                                <div class="comment-text-wrap"><div class="comment-text">{{comment.texte}}</div></div>
+                              </div>
+                              <div class="comment-actions">
+                                <a href="#pablo" class="btn btn-danger btn-simple comment-likes">
+                                    <i class="material-icons">favorite_border</i> 243
+                                </a>
+                              </div>
+                        </li>
+                    </ul>
+                </div>
+                <!-- <div class="media-area">
                     <div class="media" v-for="comment in comments">
                         <a class="pull-left" href="#pablo">
                             <div class="avatar">
@@ -132,20 +182,15 @@
                         <div class="media-body">
                             <h4 class="media-heading">{{comment.user.name}}<small>· {{comment.updated_at}}</small></h4>
                             <h6 class="text-muted"></h6>
-
                             <p>{{comment.texte}}</p>
-
                             <div class="media-footer">
-                                <!-- <a href="#pablo" class="btn btn-primary btn-simple pull-right" rel="tooltip" title="" data-original-title="Reply to Comment">
-                                    <i class="material-icons">reply</i> Reply
-                                </a> -->
                                 <a href="#pablo" class="btn btn-danger btn-simple pull-right">
                                     <i class="material-icons">favorite</i> 243
                                 </a>
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
@@ -349,4 +394,66 @@ export default {
          margin-top: 20px; 
     }
 }
+
+/*------------------------Behance---------------------------*/
+
+.comment-container {
+    margin-bottom: 25px;
+    overflow: hidden; 
+    position: relative; 
+}
+
+.rf-avatar {
+    float: left;
+    height: 50px;
+    margin-right: 20px;
+    min-height: 50px;
+    min-width: 50px;
+    width: 50px;
+    img{
+        border-radius: 50%;
+    }
+}
+
+.comment-text-container {
+    width: calc(100% - 70px);
+
+}
+.comment-text-container {
+    float: left;
+    position: relative;
+}
+
+.comment-container .user-name-link {
+    color: #3c3c3c;
+    font-size: 14px;
+    font-weight: bold;
+}
+
+.comment-date {
+    color: #a9a9a9;
+    font-size: 11px;
+}
+
+.comments-list{
+    padding: 0 20px;
+    margin-top: 20px;
+}
+
+.comment-date::before {
+    content: '\2022';
+    margin: 0 4px 0 2px;
+}
+
+.comment-actions{
+    float: left;
+    width: calc(100% - 70px);
+    margin-left: 70px;
+    .comment-likes{
+        /*margin-left: 70px;*/
+        padding: 0px;
+        margin: 0px;
+    }
+}
+
 </style>
